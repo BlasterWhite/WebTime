d4135d4c41e7dae87b4ee0f8a5c97dc99050ec99
BlasterWhite <icarambala@gmail.com>
2021-07-27 17:46:25 +0200

    Make the time live

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log("App.js is loaded");
+var hour = 12;
+var minute = 30;
+var seconde = 30;
+var play = false;
+
+function setDisplay() {
+    Time();
+    document.getElementById('time').innerHTML = `${hour}h${minute}min${seconde}sec`;
+}
+
+function Time() {
+    time = new Date()
+    hour = time.getHours();
+    minute = time.getMinutes();
+    seconde = time.getSeconds();
+}
+
+setInterval(setDisplay,1000);
